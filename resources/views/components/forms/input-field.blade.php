c8cfd63b6db5a2dfe10288601c7fe3f3973e24df
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2023-10-09 18:34:38 +0530

    FEAT: input field old value after validation error

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -26,7 +26,7 @@
 <div class="w-full">
     @if (!empty($type) && $type === 'text')
         <input type="{{ $type }}"
-            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm w-11/12 px-3 border-solid border-[#CCCCCC] focus:border-[#FFC451] focus:ring-0', 'id' => $name]) }}
+            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm w-11/12 px-3 border-solid border-[#CCCCCC] focus:border-[#FFC451] focus:ring-0', 'id' => $name, 'value' => old($name)]) }}
             name="{{ $name }}"
             id="{{ $name }}"
             placeholder="{{ $placeholder }}"
@@ -35,7 +35,7 @@
             @if ($type === 'number') inputmode="decimal" step="any" @endif>
     @elseif (!empty($type) && $type === 'number')
         <input type="{{ $type }}"
-            {{ $attributes->merge(['class' => 'h-8 p-1 w-11/12 border-[#CCCCCC] border-1 border-solid focus:ring-0 focus:border-[#FFC451] focus:border-1 bg-white overflow-hidden text-gray-500 text-xs', 'id' => $name]) }}
+            {{ $attributes->merge(['class' => 'h-8 p-1 w-11/12 border-[#CCCCCC] border-1 border-solid focus:ring-0 focus:border-[#FFC451] focus:border-1 bg-white overflow-hidden text-gray-500 text-xs', 'id' => $name, 'value' => old($name)]) }}
             name="{{ $name }}"
             id="{{ $name }}"
             placeholder="{{ $placeholder }}"
@@ -52,10 +52,10 @@
             id="{{ $name }}"
             placeholder="{{ $placeholder }}"
             @if ($disabled) disabled @endif
-            @if ($readonly) readonly @endif></textarea>
+            @if ($readonly) readonly @endif>{{ old($name) }}</textarea>
     @elseif (!empty($type) && $type === 'date')
         <input type="{{ $type }}"
-            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm px-3 border-solid border-[#CCCCCC] focus:border-[#FFC451] focus:ring-0', 'id' => $name]) }}
+            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm px-3 border-solid border-[#CCCCCC] focus:border-[#FFC451] focus:ring-0', 'id' => $name, 'value' => old($name)]) }}
             name="{{ $name }}"
             id="{{ $name }}"
             placeholder="{{ $placeholder }}"
@@ -63,7 +63,7 @@
             @if ($readonly) readonly @endif>
     @elseif (!empty($type) && $type === 'file')
         <input type="{{ $type }}"
-            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm px-3 border-solid border-[#CCCCCC] focus:outline-0 focus:border-[#FFC451] focus:ring-0', 'id' => $name, 'accept' => $acceptedFileTypes]) }}
+            {{ $attributes->merge(['class' => 'text-xs text-gray-500 rounded-sm px-3 border-solid border-[#CCCCCC] focus:outline-0 focus:border-[#FFC451] focus:ring-0', 'id' => $name, 'accept' => $acceptedFileTypes, 'value' => old($name)]) }}
             name="{{ $name }}"
             id="{{ $name }}"
             placeholder="{{ $placeholder }}"
