5e36bf7a861f9d4f5ec5ef11b3624f9d61c93dc5
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2023-10-10 11:20:08 +0530

    BUG: date formator changes and import job updated for handle duplicates and empty row

--- app/Helpers/DateFormaterHelper.php
+++ app/Helpers/DateFormaterHelper.php
@@ -8,6 +8,12 @@ class DateFormaterHelper
 {
     public static function convertDateToYMD($dateString)
     {
-        return Carbon::createFromFormat('d-M-y', $dateString)->format('Y-m-d');
+
+        //  date in the format of 01-sep-2023
+        // return Carbon::createFromFormat('d-M-y', $dateString)->format('Y-m-d');  
+
+
+        //  date in the format of 23-12-2023
+        return Carbon::createFromFormat('d-m-Y', $dateString)->format('Y-m-d');
     }
 }
--- app/Jobs/Admin/ImportUHIDcsvJob.php
+++ app/Jobs/Admin/ImportUHIDcsvJob.php
@@ -38,142 +38,158 @@ class ImportUHIDcsvJob implements ShouldQueue
      */
     public function handle()
     {
-        $rules = [
-            'policy_number' => 'required|string',
-            'emp_id' => 'required|string',
-            'policy_name' => 'required|string',
-            'uhid' => 'required|string',
-            'insured_name' => 'required|string',
-            'age' => 'required|string',
-            'dob' => 'required|date',
-            'gender' => 'required|string',
-            'relationship' => 'required|string',
-            'doj' => 'required|date',
-            'doc' => 'required|date',
-            'doe' => 'required|date',
-            'sum_insured' => 'nullable|numeric',
-            'status' => 'required|string',
-            'insurer' => 'required|string',
-        ];
+        try {
+            $rules = [
+                'policy_number' => 'required|string',
+                'emp_id' => 'required|string',
+                'policy_name' => 'required|string',
+                'uhid' => 'required|string',
+                'insured_name' => 'required|string',
+                'age' => 'required|string',
+                'dob' => 'required|date',
+                'gender' => 'required|string',
+                'relationship' => 'required|string',
+                'doj' => 'required|date',
+                'doc' => 'required|date',
+                'doe' => 'required|date',
+                'sum_insured' => 'nullable|numeric',
+                'status' => 'required|string',
+                'insurer' => 'required|string',
+            ];
 
 
-        // Read the file
-        $filePath = storage_path('app/' . $this->filePath);
-        $rows = array_map('str_getcsv', file($filePath));
+            // Read the file
+            $filePath = storage_path('app/' . $this->filePath);
+            $rows = array_map('str_getcsv', file($filePath));
 
-        // Remove empty rows
-        $rows = array_filter($rows, function ($row) {
-            return !empty(array_filter($row, 'trim'));
-        });
+            // Remove empty rows
+            $rows = array_filter($rows, function ($row) {
+                return !empty(array_filter($row, 'trim'));
+            });
 
-        // Reindex rows after filtering
-        $rows = array_values($rows);
-        // Remove the header row
-        $header = array_shift($rows);
+            // Reindex rows after filtering
+            $rows = array_values($rows);
+            // Remove the header row
+            $header = array_shift($rows);
 
-        // Initialize skipped rows array with header
-        $skippedRows = [$header];
+            // Initialize skipped rows array with header
+            $skippedRows = [$header];
 
-        // Loop through the data rows and create new candidates and users
-        foreach ($rows as $row) {
-            // Combine the header with the row data
-            $clientUHIDData = array_combine($header, $row);
+            $duplicateRows = [$header];
 
-            // Validate the required fields
-            $validator = Validator::make($clientUHIDData, $rules);
-            if ($validator->fails()) {
-                // Add skipped rows to array
-                $skippedRows[] = $row;
-                continue;
-            }
-            // dd($clientUHIDData);
+            // Loop through the data rows and create new candidates and users
+            foreach ($rows as $row) {
+                // Combine the header with the row data
+                $clientUHIDData = array_combine($header, $row);
 
-            $uhid = Uhid::create([
-                'uuid' => UuidGeneratorHelper::generateUniqueUuidForTable('uhids'),
-                'policy_number' => $clientUHIDData['policy_number'],
-                'emp_id' => $clientUHIDData['emp_id'],
-                'policy_name' => $clientUHIDData['policy_name'],
-                'uhid' => $clientUHIDData['uhid'],
-                'insured_name' => $clientUHIDData['insured_name'],
-                'age' => $clientUHIDData['age'],
-                'dob' => $clientUHIDData['dob'],
-                // 'dob' => DateFormaterHelper::convertDateToYMD($clientUHIDData['dob']),
-                'gender' => $clientUHIDData['gender'],
-                'relationship' => $clientUHIDData['relationship'],
-                'doj' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doj']),
-                'doc' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doc']),
-                'doe' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doe']),
-                'sum_insured' => $clientUHIDData['sum_insured'],
-                'status' => $clientUHIDData['status'],
-                'insurer' => $clientUHIDData['insurer'],
-            ]);
-
-            // dd($uhid);
-            // Check if user already exists
-            // $userEmail = $candidateData['email'];
-            // $user = User::where('email', $userEmail)->first();
-
-            // if (!$user) {
-            //     // Create a new user record
-            //     $tempPassword = Str::random(8);
-            //     $user = User::create([
-            //         'role_id' => 3,
-            //         'uuid' => UuidGeneratorHelper::generateUniqueUuidForTable('users'),
-            //         'first_name' => $candidateData['first_name'],
-            //         'last_name' => $candidateData['last_name'],
-            //         'email' => $userEmail,
-            //         'password' => Hash::make($tempPassword),
-            //         'verification_token' => Str::random(40),
-            //     ]);
-            // } else {
-            //     // Use existing user record
-            //     $tempPassword = null;
-            // }
-
-            // Check if candidate already exists
-            // if (Candidate::where('user_id', $user->id)->exists()) {
-            //     // Skip duplicate candidate
-            //     $skippedRows[] = $row;
-            //     continue;
-            // }
-
-            // Create a new candidate record
-            // $candidateData['uuid'] = UuidGeneratorHelper::generateUniqueUuidForTable('candidates');
-            // $candidateData['user_id'] = $user->id;
-            // $candidateData['agency_id'] = $agency_id;
-            // $candidateData['is_profile_completed'] = false;
-
-            try {
-                // Candidate::create($candidateData);
-            } catch (\Illuminate\Database\QueryException $e) {
-                // Skip duplicate entry exception
-                if (strpos($e->getMessage(), 'Duplicate entry') !== false) {
+                // Validate the required fields
+                $validator = Validator::make($clientUHIDData, $rules);
+                if ($validator->fails()) {
+                    // Add skipped rows to array
                     $skippedRows[] = $row;
                     continue;
                 }
-                return redirect()->back()->with('error', 'Database error: ' . $e->getMessage());
+                $sumInsured = $clientUHIDData['sum_insured'] !== '' ? floatval($clientUHIDData['sum_insured']) : 0;
+
+                $clientUhid = $clientUHIDData['uhid'];
+                // $client = Uhid::where('uhid', $userUhid)->first();
+
+                if (Uhid::where('uhid', $clientUhid)->exists()) {
+                    // Skip duplicate candidate
+                    $duplicateRows[] = $row;
+                    continue;
+                }
+
+                $uhid = Uhid::create([
+                    'uuid' => UuidGeneratorHelper::generateUniqueUuidForTable('uhids'),
+                    'policy_number' => $clientUHIDData['policy_number'],
+                    'emp_id' => $clientUHIDData['emp_id'],
+                    'policy_name' => $clientUHIDData['policy_name'],
+                    'uhid' => $clientUHIDData['uhid'],
+                    'insured_name' => $clientUHIDData['insured_name'],
+                    'age' => $clientUHIDData['age'],
+                    'dob' => DateFormaterHelper::convertDateToYMD($clientUHIDData['dob']),
+                    'gender' => $clientUHIDData['gender'],
+                    'relationship' => $clientUHIDData['relationship'],
+                    'doj' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doj']),
+                    'doc' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doc']),
+                    'doe' => DateFormaterHelper::convertDateToYMD($clientUHIDData['doe']),
+                    'sum_insured' => $sumInsured,
+                    'status' => $clientUHIDData['status'],
+                    'insurer' => $clientUHIDData['insurer'],
+                ]);
+
+                // Check if user already exists
+                // $userEmail = $candidateData['email'];
+                // $user = User::where('email', $userEmail)->first();
+
+                // if (!$user) {
+                //     // Create a new user record
+                //     $tempPassword = Str::random(8);
+                //     $user = User::create([
+                //         'role_id' => 3,
+                //         'uuid' => UuidGeneratorHelper::generateUniqueUuidForTable('users'),
+                //         'first_name' => $candidateData['first_name'],
+                //         'last_name' => $candidateData['last_name'],
+                //         'email' => $userEmail,
+                //         'password' => Hash::make($tempPassword),
+                //         'verification_token' => Str::random(40),
+                //     ]);
+                // } else {
+                //     // Use existing user record
+                //     $tempPassword = null;
+                // }
+
+                // Check if candidate already exists
+                // if (Candidate::where('user_id', $user->id)->exists()) {
+                //     // Skip duplicate candidate
+                //     $skippedRows[] = $row;
+                //     continue;
+                // }
+
+                // Create a new candidate record
+                // $candidateData['uuid'] = UuidGeneratorHelper::generateUniqueUuidForTable('candidates');
+                // $candidateData['user_id'] = $user->id;
+                // $candidateData['agency_id'] = $agency_id;
+                // $candidateData['is_profile_completed'] = false;
+
+                try {
+                    // Candidate::create($candidateData);
+                } catch (\Illuminate\Database\QueryException $e) {
+                    // Skip duplicate entry exception
+                    if (strpos($e->getMessage(), 'Duplicate entry') !== false) {
+                        $skippedRows[] = $row;
+                        continue;
+                    }
+                    return redirect()->back()->with('error', 'Database error: ' . $e->getMessage());
+                }
+
+                // if ($tempPassword) {
+                //     Send notification only if new user is created
+                //     SendImportCandidateNotificationJob::dispatch($userEmail,  $tempPassword);
+                // }
             }
 
-            // if ($tempPassword) {
-            //     Send notification only if new user is created
-            //     SendImportCandidateNotificationJob::dispatch($userEmail,  $tempPassword);
+            dd($skippedRows,$duplicateRows);
+            // Delete the temporary file after processing
+            Storage::delete($this->filePath);
+
+            // if (count($skippedRows) > 1) {
+            //     $csv = fopen($this->skippedRowsFilePath, 'w');
+            //     foreach ($skippedRows as $row) {
+            //         fputcsv($csv, $row);
+            //     }
+            //     fclose($csv);
+
+            //     $agencyUser = Agency::where('id', $agency_id)->first();
+
+            //     $agencyUser->skipped_candidates_url = $this->skippedRowsFilePath;
+            //     $agencyUser->save();
             // }
+            return redirect()->back()->with('success', 'Data Uploaded Successfully');
+        } catch (\Exception $e) {
+
+            return redirect()->back()->with('error', 'Database error: ' . $e->getMessage());
         }
-
-        // Delete the temporary file after processing
-        Storage::delete($this->filePath);
-
-        // if (count($skippedRows) > 1) {
-        //     $csv = fopen($this->skippedRowsFilePath, 'w');
-        //     foreach ($skippedRows as $row) {
-        //         fputcsv($csv, $row);
-        //     }
-        //     fclose($csv);
-
-        //     $agencyUser = Agency::where('id', $agency_id)->first();
-
-        //     $agencyUser->skipped_candidates_url = $this->skippedRowsFilePath;
-        //     $agencyUser->save();
-        // }
     }
 }
