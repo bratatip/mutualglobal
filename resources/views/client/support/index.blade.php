691856a1b7c38d5e0471352d51952e9fe115bdd5
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2024-06-21 14:14:18 +0530

    UI: Support page outer card removed

--- resources/views/client/support/index.blade.php
+++ resources/views/client/support/index.blade.php
@@ -1,42 +1,37 @@
 @extends('client.layouts.app')
 
 @section('content')
-    <x-common.card>
-        @slot('card_content')
-            <div class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
-                 id="modal_logout"
-                 style="z-index: 9999 !important">
-                <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+    <div class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
+         id="modal_logout"
+         style="z-index: 9999 !important">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
-                <div
-                     class="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto modal_604px border_bg" style="margin-inline: 10px;">
+        <div class="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto modal_604px border_bg"
+             style="margin-inline: 10px;">
 
-                    <!-- Modal content -->
-                    <div class="modal-content py-4 text-left px-6">
+            <!-- Modal content -->
+            <div class="modal-content py-4 text-left px-6">
 
-                        <div class="flex justify-end">
-                            <p
-                               class="modal-close cursor-pointer px-3 py-1 bg-[#F5F5F5] hover:text-red-500 rounded-full shadow-xl">
-                                <a href="{{ route('Client.dashboard') }}">x</a>
-                            </p>
-                        </div>
-
-                        <img src="{{ asset('images/app/support.jpg') }}"
-                             alt="..."
-                             class="relative">
-                        <div class="mt-[50px] max-md:mt-[10px]"></div>
-
-                        <div class="mt-[50px] max-md:mt-[10px]"></div>
-
-                    </div>
-
+                <div class="flex justify-end">
+                    <p class="modal-close cursor-pointer px-3 py-1 bg-[#F5F5F5] hover:text-red-500 rounded-full shadow-xl">
+                        <a href="{{ route('Client.dashboard') }}">x</a>
+                    </p>
                 </div>
+
+                <img src="{{ asset('images/app/support.jpg') }}"
+                     alt="..."
+                     class="relative">
+                <div class="mt-[50px] max-md:mt-[10px]"></div>
+
+                <div class="mt-[50px] max-md:mt-[10px]"></div>
+
             </div>
-        @endslot
-    </x-common.card>
+
+        </div>
+    </div>
 @endsection
 <style>
-    .modal-container{
+    .modal-container {
         max-width: 750px !important;
         /* max-height: 300px !important; */
     }
